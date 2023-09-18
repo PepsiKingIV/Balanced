d5c1579891f526c25bdcaa5cddbd06970c2061f3
Alexandr Fonov <FonovAD@Gmail.com>
2023-09-18 22:05:10 +0300

    Add consent about cookies
    A page with a description of cookies has been created.
    A pop-up window with consent about cookies
    has also been created

--- main/views.py
+++ main/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    print(request)
+    
     return render(request, "main/index.html")
+
+def consent(request):
+    return render(request, 'main/consent.html')
